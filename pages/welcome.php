5bd1143b236e96f1e6944b11d23f8df8ca8bef0e
GTM-Winter <dimastriana03@gmail.com>
2022-04-26 08:33:57 +0700

    add file welcome

---
+++ pages/welcome.php
@@ -0,0 +1,19 @@
+<?php
+$template="welcome";
+$konten="
+    <div class='container-fluid d-flex justify-content-center'>
+      <img src='assets/welcome/img/1.png' alt='welcome' width='350px' height='240px'>
+    </div>
+    <div class='container'>
+      <h2 class='text-left'>Hello, Welcome to Papada</h2>
+       <q>
+       tongkrongan kami bukan tongkrongan pecundang,kami siap membuktikan, melawan tongkrongan kampungan
+       </q>
+    </div>
+    <div class='d-grid gap-2 mt-5'>
+    <a href='?page=login' class='cta'>
+    <button type='submit' class='p-4 m-3 btn btn-primary rounded-pill'>Get Started<i class='fa fa-arrow-right'></i></button>
+    </a>
+  </div>
+";
+?>
