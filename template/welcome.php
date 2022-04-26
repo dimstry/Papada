2ef314a46adbcaa455a52044cad0bc9d0eb3483d
GTM-Winter <dimastriana03@gmail.com>
2022-04-26 08:33:27 +0700

    add template for welcome

---
+++ template/welcome.php
@@ -0,0 +1,27 @@
+<?php 
+echo "
+<!doctype html>
+<html lang='en'>
+  <head>
+    <!-- Required meta tags -->
+    <meta charset='utf-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <!-- Bootstrap CSS -->
+    <link href='assets/bootstrap/css/bootstrap.min.css' rel='stylesheet'>
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>
+    <link rel='stylesheet' href='assets/welcome/style.css'>
+
+    <title>Welcome</title>
+  </head>
+ <body>
+  $konten
+      <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src='assets/bootstrap/js/bootstrap.bundle.min.js'></script>
+
+
+  </body>
+</html>
+";
+?>
